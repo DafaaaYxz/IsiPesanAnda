c0e490d568057ac194fc476be9c2cbc0583a8c9a
DafaaaYxz <zeuz8686@gmail.com>
2025-12-24 14:19:20 +0700

    Update router.js

--- app/api/simpan/router.js
+++ app/api/simpan/router.js
@@ -1,13 +1,10 @@
-import { konekKeDatabase } from "@/lib/db";
+
+import { konekDb } from "@/lib/db";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
-  const { penerima, isi } = await req.json();
-  const db = await konekKeDatabase();
-  await db.collection("pesan").insertOne({
-    penerima,
-    isi,
-    tgl: new Date()
-  });
-  return NextResponse.json({ sukses: true });
+  const { untuk, pesan } = await req.json();
+  const db = await konekDb();
+  await db.collection("inbox").insertOne({ untuk, pesan, tgl: new Date() });
+  return NextResponse.json({ ok: true });
 }
